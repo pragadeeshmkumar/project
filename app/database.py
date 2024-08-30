bfea0d2aebd6942af3e9f46c134eabfde0b45d8a
Pragadeesh M <112326747+pragadeeshmkumar@users.noreply.github.com>
2024-08-30 20:03:04 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://palcode_dev:palcode123!#%@91.108.110.211:5433/blog_platform"
+SQLALCHEMY_DATABASE_URL = " "
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
